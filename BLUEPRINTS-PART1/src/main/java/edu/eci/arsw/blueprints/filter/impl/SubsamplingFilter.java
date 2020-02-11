fbda3e0c0a2a34d30613f6bdf75fa4a7bdaca57e
Andres Felipe Marcelo <53924254+AndresMarcelo7@users.noreply.github.com>
2020-02-11 16:02:56 -0500

    Implementacionsegundo filtro

--- BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/filter/impl/SubsamplingFilter.java
+++ BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/filter/impl/SubsamplingFilter.java
@@ -2,9 +2,11 @@ package edu.eci.arsw.blueprints.filter.impl;
 
 import edu.eci.arsw.blueprints.filter.FilterBlueprints;
 import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.model.Point;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 @Component
@@ -13,6 +15,23 @@ public class SubsamplingFilter implements FilterBlueprints {
 
     @Override
     public Blueprint filterBlueprint(Blueprint bp) {
-        return null;
+        ArrayList<Point> aux = new ArrayList<Point>();
+
+        for (int i=0; i<bp.getPoints().size(); i++) {
+            if (i%2==0){
+                aux.add(bp.getPoints().get(i));
+            }
+        }
+        Point[] rta = toStaticList(aux);
+        return new Blueprint(bp.getAuthor(),bp.getName(),rta);
+
+    }
+
+    private Point[] toStaticList(ArrayList<Point> arreglo){
+        Point[] rta = new Point[arreglo.size()];
+        for(int i=0; i<arreglo.size(); i++ ){
+            rta[i] = arreglo.get(i);
+        }
+        return rta;
     }
 }
